63150c323f0b56a6171877ff494daa307be64fd0
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-03-02 22:07:57 +0545

    i made the player move

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,29 +1,38 @@
 extends CharacterBody2D
 
-# 1. Setup your constants
 const SPEED = 400.0
-const JUMP_VELOCITY = -500.0 # Negative is UP in Godot
-
-# 2. Get the gravity from Project Settings (standard is 980)
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+const JUMP_VELOCITY = -500.0
+@onready var sprite = $AnimatedSprite2D
+#getting the gravity
+var gravity = 1000
 
 func _physics_process(delta):
-	# 3. Add Gravity if not on the floor
+	# adding gravity for the sprite
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		sprite.play("jump")
 
-	# 4. Handle Jump (Using your 'space' action)
+	# Making the thing jump 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# 5. Get direction using your 'left' and 'right' actions
-	var direction = Input.get_axis("left", "right")
+	# this gets the direction as input as the -1,0,1 resp..
+	var direction = Input.get_axis("Left", "Right")
 	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		# Smoothly stop when no button is pressed
+		# Smoothly stop the character (Friction)
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	# 6. Apply movement and check for the floor
+	#flipping the velcoity
+	if direction>0:
+		sprite.flip_h = false
+	if direction<0:
+		sprite.flip_h = true
+	if is_on_floor():
+		if direction != 0:
+			sprite.play("run")
+		else :
+			sprite.play("idle")
+	# 4. Final step: Apply the movement
 	move_and_slide()
