d0dcaa4d1780c6b83f3f1c97785dd5a1b28ed744
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-03-04 13:48:28 +0545

    Just made the log respwaning feature

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 @onready var platforms = preload("res://Scences/floor.tscn")
+@onready var music =$Music_manager
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	music.play()
 	pass # Replace with function body.
 
 
---
+++ scripts/spwanner.gd
@@ -0,0 +1,30 @@
+extends Marker2D
+
+@export var log_scene: PackedScene 
+@onready var player = $"../CharacterBody2D"
+
+var spawn_distance = 250.0
+var next_spawn_y = 0.0
+
+func _ready():
+	if player:
+		next_spawn_y = player.global_position.y - spawn_distance
+
+func _process(_delta):
+	if player:
+		if player.global_position.y < next_spawn_y + 600:
+			spawn_log()
+			var difficulty_jump = clamp(abs(player.global_position.y) / 5000, 0, 100)
+			next_spawn_y -= (spawn_distance + difficulty_jump)
+			
+
+func spawn_log():
+	var new_log = log_scene.instantiate()
+	var random_x = randf_range(-200, 200)
+	new_log.global_position = Vector2(random_x, next_spawn_y)
+	
+	# Random Modulation (The color tint)
+	new_log.modulate = Color(randf(), randf(), randf(), 1.0)
+	
+	# Add the log to the main scene
+	get_parent().add_child(new_log)
