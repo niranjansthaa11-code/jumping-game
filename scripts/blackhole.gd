ee4c301f615cf2cee4490739c45947b06f751017
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-03-04 20:32:03 +0545

    added some music

--- scripts/blackhole.gd
+++ scripts/blackhole.gd
@@ -1,19 +1,42 @@
 extends Node2D
 
-@export var speed =100
-@export var acceleration = 5
-@export var max_speed = 400
+# Export variables so you can edit them in the Inspector
+@export var speed = 100.0
+@export var acceleration = 2.0
+@export var max_speed = 300
+
+@onready var player = get_node("../CharacterBody2D")
+@onready var dead_music =$Main_one/AudioStreamPlayer2
 
 func _process(delta: float) -> void:
+	if not player: return
+
+	# 1. Calculate how far the player is
+	var distance = position.y - player.global_position.y
 	
-	if speed < max_speed:
-		speed += acceleration * delta
-		position.y -= speed * delta
+	# 2. TARGET SPEED: If the player is far, go fast. If close, go slow.
+	# This "Target" will be our max speed limit.
+	var target_speed = 350.0 
+	
+	if distance > 900:
+		# Catch up quickly if the player is pro
+		speed = move_toward(speed, 500.0, acceleration * 5 * delta)
+	elif distance < 400:
+		# Slow down to a "crawl" if the player is about to die
+		speed = move_toward(speed, 150.0, acceleration * 10 * delta)
+	else:
+		# Normal cruising speed
+		speed = move_toward(speed, target_speed, acceleration * delta)
+
+	# 3. Apply the movement
+	position.y -= speed * delta
+	
+
 
 func _on_main_one_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		print("player is in void")		
+	dead_music.play()
+	if body.is_in_group("player") or body.name == "CharacterBody2D":
+		print("Player consumed by the void!")		
 		await get_tree().create_timer(0.5).timeout
 		get_tree().reload_current_scene()
-func _ready() -> void:
-	pass 
+	pass # Replace with function body.
