d922518fc75f2dd8d65d9d245e8029ca105afba8
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-03-05 19:16:01 +0545

    I added the main_menu and the exit_menu scence

---
+++ Scences/exit_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scences/main_menu.tscn")
+	pass # Replace with function body.
+	
+
+
+func _on_restart_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scences/game_manager.tscn")
+	
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
