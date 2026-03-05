fc0cf630bfe009c880cd24fb13079d281be6aab7
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-03-05 20:06:25 +0545

    made the exit menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -14,3 +14,8 @@ func _process(delta: float) -> void:
 func _on_start_button_pressed() -> void: 
 	get_tree().change_scene_to_file("res://Scences/game_manager.tscn")
 	pass # Replace with function body.
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
